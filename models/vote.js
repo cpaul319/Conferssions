f927f27a9764331a04842a83619b7092f5384b08
Nitza <rondon.nitza@gmail.com>
2019-05-05 00:04:46 -0400

    Models for db

---
+++ models/vote.js
@@ -0,0 +1,31 @@
+// // creates the Voting table
+module.exports = function(sequelize, DataTypes) {
+  var Vote = sequelize.define("Vote", {
+    // Giving the Author model a name of type STRING
+    Like: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    },
+    Comment: {
+      type: DataTypes.STRING
+    }
+  });
+
+  // Associating a Vote to a User
+  // Does not have to vote
+  Vote.associate = function(models) {
+    Vote.belongsTo(models.User, {
+      allowNull: true
+    });
+  };
+
+  //   // Associating Votes with with a confessions
+  //   // Does not have to a confessions
+  Vote.associate = function(models) {
+    Vote.belongsTo(models.Confession, {
+      allowNull: false
+    });
+  };
+  return Vote;
+};
