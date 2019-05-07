e8f32d06d590a8cc5af64a3132421003dd9b2e18
Nitza <rondon.nitza@gmail.com>
2019-05-07 16:53:01 -0400

    Updated controllers with tablename

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const db = require('../models');
 module.exports = {
     findAll: function (req, res) {
         db.User.findAll({
-            include: [db.Post]
+            include: [db.Confession]
         }).then(function (dbUser) {
             res.json(dbUser);
         });
@@ -13,7 +13,8 @@ module.exports = {
             where: {
                 id: req.params.id
             },
-            include: [db.Post]
+            include: [db.Confession]
+            
         }).then(function (dbUser) {
             res.json(dbUser);
         });
