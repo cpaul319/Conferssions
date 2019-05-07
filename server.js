d2f9de911ce11f4374bc67844f5abb079ed16727
FrankCalovini <46719590+FrankCalovini@users.noreply.github.com>
2019-05-07 15:14:52 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-var passport = require('passport');
+var session = require("express-session");
+var passport = require("passport");
 var db = require("./models");
-
 var app = express();
 var PORT = process.env.PORT || 3000;
 
