27e46a40e445ffe85577798e54cdbee5d8924817
FrankCalovini <46719590+FrankCalovini@users.noreply.github.com>
2019-05-07 07:53:19 -0400

    Add files via upload

---
+++ controllers/voteController.js
@@ -0,0 +1,25 @@
+var Q = require("q");
+var Idea = require("../model.js");
+
+module.exports = {
+  truevote: function(req, res, next) {
+    updateVoteCount(req, res, 1);
+  },
+  falsevote: function(req, res, next) {
+    updateVoteCount(req, res, -1);
+  }
+};
+  
+var updateVoteCount = function(req, res, changeValue) {
+var updateVotes = Q.nbind(Idea.findOneAndUpdate, Idea);
+var query = { title: req.body.title };
+    
+  updateVotes(query, { $inc: { votes: changeValue } })
+    .then(function (idea) {
+        res.send(idea);
+      })
+    .fail(function (err) {
+      console.log(err);
+      next(err);
+    });
+};
