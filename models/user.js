c8ac998cf3faeb520698f774c431721115c3b696
FrankCalovini <46719590+FrankCalovini@users.noreply.github.com>
2019-05-07 15:16:11 -0400

    Passport updates

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+var bcrypt = require("bcrypt-nodejs");
+
 // creates the user table
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
@@ -17,6 +19,18 @@ module.exports = function(sequelize, DataTypes) {
     }
   });
 
+  //This will check if an unhashed password entered by the 
+  //user can be compared to the hashed password stored in our database
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a User is created, we will automatically hash their password
+  User.hook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  });
+  return User;
+  
   // Associating User with multiple Confessions
   // When User is deleted, deleted any associated confessions
   User.associate = function(models) {
