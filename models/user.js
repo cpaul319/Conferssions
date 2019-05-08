04f8dbfe07612ab421f394c5718dd56153d6f463
Nitza <rondon.nitza@gmail.com>
2019-05-08 17:06:24 -0400

    Updated models/user.js file with passport code

--- models/user.js
+++ models/user.js
@@ -19,18 +19,6 @@ module.exports = function(sequelize, DataTypes) {
     }
   });
 
-  //This will check if an unhashed password entered by the 
-  //user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
-  };
-  // Hooks are automatic methods that run during various phases of the User Model lifecycle
-  // In this case, before a User is created, we will automatically hash their password
-  User.hook("beforeCreate", function(user) {
-    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
-  });
-  return User;
-  
   // Associating User with multiple Confessions
   // When User is deleted, deleted any associated confessions
   User.associate = function(models) {
@@ -46,6 +34,17 @@ module.exports = function(sequelize, DataTypes) {
       onDelete: "cascade"
     });
   };
+
+// Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+User.prototype.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+// Hooks are automatic methods that run during various phases of the User Model lifecycle
+// In this case, before a User is created, we will automatically hash their password
+User.hook("beforeCreate", function(user) {
+  user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+});
+
 
   return User;
 };
